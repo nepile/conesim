b34d585b96c2f2b93439bd3986833568dcaf475c
Neville Jeremy <nevillejro.laia@gmail.com>
2026-09-14 00:24:05 +0700

    feat: implement configuration namespace management

--- include/core/Configuration.hpp
+++ include/core/Configuration.hpp
@@ -11,8 +11,21 @@ class Configuration {
 private:
     static std::unordered_map<std::string, std::string> properties;
 
+    std::string namespaceName;
+    std::string secondaryNamespace;
+
+    std::stack<std::string> oldNamespaces;
+    std::stack<std::string> oldSecondaryNamespaces;
+
 public:
     Configuration();
+    explicit Configuration(const std::string& namespaceName);
+
+    void setNamespace(const std::string& name);
+    void restoreNamespace();
+
+    void setSecondaryNamespace(const std::string& name);
+    void restoreSecondaryNamespace();
 };
 
 }
--- src/core/Configuration.cpp
+++ src/core/Configuration.cpp
@@ -9,4 +9,54 @@ namespace conesim {
 std::unordered_map<std::string, std::string>
     Configuration::properties;
 
+
+Configuration::Configuration()
+    :   namespaceName(""),
+        secondaryNamespace("")
+{}
+
+Configuration::Configuration(const std::string& namespaceName)
+    :   namespaceName(namespaceName),
+        secondaryNamespace("")
+{}
+
+void Configuration::setNamespace(const std::string& name)
+{
+    oldNamespaces.push(namespaceName);
+    namespaceName = name;
+}
+
+void Configuration::restoreNamespace()
+{
+    if (oldNamespaces.empty())
+    {
+        throw std::runtime_error(
+            "No previous namespace to restore"
+        );
+    }
+
+    namespaceName = oldNamespaces.top();
+    oldNamespaces.pop();
+}
+
+void Configuration::setSecondaryNamespace(const std::string& name)
+{
+    oldSecondaryNamespaces.push(secondaryNamespace);
+    secondaryNamespace = name;
+}
+
+void Configuration::restoreSecondaryNamespace()
+{
+    if (oldSecondaryNamespaces.empty())
+    {
+        throw std::runtime_error(
+            "No previous secondary namespace to restore"
+        );
+    }
+
+    secondaryNamespace = oldSecondaryNamespaces.top();
+    oldSecondaryNamespaces.pop();
+}
+
+
 }
