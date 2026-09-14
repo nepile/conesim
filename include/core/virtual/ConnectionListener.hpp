c64aac13f5f35896a8ddeb2444337fd331366b8c
Neville Jeremy <nevillejro.laia@gmail.com>
2026-09-14 22:24:12 +0700

    bugfix: change parameter access to read-only on ConnectionListener

--- include/core/virtual/ConnectionListener.hpp
+++ include/core/virtual/ConnectionListener.hpp
@@ -31,7 +31,7 @@ public:
      * @param host1 The first host involved in the connection.
      * @param host2 The second host involved in the connection.
      */
-    virtual void hostsConnected(Host& host1, Host& host2) = 0;
+    virtual void hostsConnected(const Host& host1, const Host& host2) = 0;
 
     /**
      * @brief Callback invoked when an active connection between two hosts is severed.
@@ -39,7 +39,7 @@ public:
      * @param host1 The first host involved in the disconnected link.
      * @param host2 The second host involved in the disconnected link.
      */
-    virtual void hostDisconnected(Host& host1, Host& host2) = 0;
+    virtual void hostDisconnected(const Host& host1, const Host& host2) = 0;
 };
 
 } // namespace conesim
