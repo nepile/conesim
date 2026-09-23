587b7cfc6ab682ccc2335e9aee646ba89e8e7455
Frathol <michaelmosses35746@gmail.com>
2026-09-23 16:37:59 +0700

    bugfix : fix CMakeList and add documentation for DTNHost

--- include/core/DTNHost.hpp
+++ include/core/DTNHost.hpp
@@ -1,7 +1,7 @@
 /**
  * @file DTNHost.hpp
  * @brief Header for the DTNHost class in ONE Simulator C++ port.
- * @author Neville / Team
+ * @author Neville
  * @date September 2026
  * 
  * Copyright 2010 Aalto University, ComNet
@@ -20,9 +20,6 @@
 #include "core/Coord.hpp"
 #include "routing/community/Duration.hpp" 
 
-// ============================================================================
-// Correct Namespace Forward Declarations
-// ============================================================================
 namespace movement {
     class MovementModel;
     class Path;
@@ -76,8 +73,22 @@ namespace core {
         // Host connects to bus, but does not own it
         ModuleCommunicationBus* comBus;
 
+        /**
+         * @brief Returns a new network interface address and increments it.
+         * @return The next address.
+         */
         static int getNextAddress();
+
+        /**
+         * @brief Set a router for this host
+         * @param router The router to set
+         */
         void setRouter(std::shared_ptr<routing::MessageRouter> router);
+
+        /**
+         * @brief Sets the next destination and speed to correspond the next waypoint on the path.
+         * @return True if there was a next waypoint to set, false if node still should wait
+         */
         bool setNextWaypoint();
 
     public:
@@ -103,6 +114,17 @@ namespace core {
         // CONSTRUCTOR & DESTRUCTOR
         // ====================================================================
         
+        /**
+         * @brief Creates a new DTNHost.
+         *
+         * @param msgLs Message listeners
+         * @param movLs Movement listeners
+         * @param groupId GroupID of this host
+         * @param interf List of NetworkInterfaces for the class
+         * @param comBus Module communication bus object
+         * @param mmProto Prototype of the movement model of this host
+         * @param mRouterProto Prototype of the message router of this host
+         */
         DTNHost(const std::vector<MessageListener*>& msgLs,
                 const std::vector<MovementListener*>& movLs,
                 const std::string& groupId, 
@@ -117,16 +139,41 @@ namespace core {
         // CORE METHODS
         // ====================================================================
 
+        /**
+         * @brief Reset the host and its interfaces (Static context)
+         */
         static void reset();
+
+
+        /**
+         * @brief Returns true if this node is active (false if not)
+         * @return true if this node is active
+         */
         bool isActive() const;
         
+        /**
+         * @brief Returns the router of this host
+         * @return the router of this host
+         */
         std::shared_ptr<routing::MessageRouter> getRouter() const;
+
+        /**
+         * @brief Returns the network-layer address of this host.
+         */
         int getAddress() const;
         void setAddress(int address);
+
+        /**
+         * @brief Returns this hosts's ModuleCommunicationBus
+         */
         ModuleCommunicationBus* getComBus() const;
 
         void connectionUp(Connection* con);
         void connectionDown(Connection* con);
+
+        /**
+         * @brief Returns a copy of the list of connections this host has with other hosts
+         */
         std::vector<Connection*> getConnections() const;
 
         Coord getLocation() const;
@@ -173,15 +220,33 @@ namespace core {
         // COMPARISON & EQUALITY
         // ====================================================================
 
+        /**
+         * @brief Checks if a host is the same as this host by comparing pointer memory address.
+         */
         bool equals(const DTNHost* otherHost) const;
+
+        /**
+         * @brief Compares two DTNHosts by their addresses. (Parity with Java's compareTo)
+         */
         int compareTo(const DTNHost* h) const;
+
+        /**
+         * @brief C++ idiomatic way to implement Comparable<DTNHost>.
+         */
         bool operator<(const DTNHost& other) const;
 
         // ====================================================================
         // ADDITIONAL METHODS
         // ====================================================================
 
+        /**
+         * @brief additional method for add Duration to list
+         */
         void addDuration(const routing::community::Duration& dur);
+
+        /**
+         * @brief additional method for get string for representation of intervals
+         */
         std::string getNodeIntervals() const;
     };
 
